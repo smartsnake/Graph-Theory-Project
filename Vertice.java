0c305f81190dd1b5d70f5e90c2676ec583d3e653
Brian Dearing <smartsnake1@gmail.com>
2019-01-03 08:48:57 -0600

    Added distance
    Calculates the distance between vertices and prints it on the edge. I
    need to test the changes, was coded on an Ipad.

--- Vertice.java
+++ Vertice.java
@@ -1,7 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Color;
 import java.util.ArrayList;
-
+import java.lang.Math;
 
 public class Vertice{
     int id;
@@ -22,13 +22,22 @@ public class Vertice{
         g.drawString("" + this.id, (this.x + (this.circleSize/2) - 2), this.y + (this.circleSize - 2));
     }
     public void drawEdges(Graphics g){
+        int[] dist = getDistance();
         for(int z = 0; z < this.Edges.size(); z++){
             g.setColor(Color.BLACK);
+            g.drawString("" + dist[z], this.Edges.get(z).x, this.get(z).y);
             g.drawLine(this.x, this.y, this.Edges.get(z).x, this.Edges.get(z).y);
         }
         
     }
-    // public Edge addEdge(Vertice b){
-    //     return new Edge(this, b);
-    // }
+    public float dist(int x2, int y2){
+        return Math.sqrt((this.x - x2) * (this.x - x2) + (this.y - y2) * (this.y - y2));
+    }
+    public int[] getDistance(){
+        int[] dist = new int[Edges.size()];
+        for(int x = 0; x < dist.length; x++){
+                dist[x] = dist(Edges.get(x).x, Edges.get(x).y);
+        }
+        return dist;
+    }
 }
